4a1a7b2b565a3be775488d19fd22a9573acd6cd1
Chenji-s <s2671252@ed.ac.uk>
2024-09-19 11:11:24 +0100

    Add files via upload

---
+++ 测试.R
@@ -0,0 +1,11 @@
+# 手动输入3x3矩阵
+matrix_3x3 <- matrix(c(1, 2, 3, 
+                       4, 5, 6, 
+                       7, 8, 10), 
+                     nrow = 3, byrow = TRUE)
+
+
+# 显示矩阵
+print("输入的矩阵为：")
+print(matrix_3x3)
+
