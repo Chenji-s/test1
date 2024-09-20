cedba9601e1699fa1e0aa242907ca3dd2e17f56c
Chenji-s <s2671252@ed.ac.uk>
2024-09-20 21:45:38 +0100

    2-2

--- 教学2.py
+++ 教学2.py
@@ -1,1 +1,16 @@
-print(2)
+'''
+
+'''
+'''
+type()
+该函数用于查看对象的类型。以下是代码示例：
+'''
+x = 2
+y = 3.5
+z = 'abc'
+
+print(type(x), type(y), type(z))
+
+#type(x) 返回 int，表示x是一个整数类型。
+#type(y) 返回 float，表示y是一个浮点数类型。
+#type(z) 返回 str，表示z是一个字符串类型。
