88ec80a294f376023551eebb173e9493bff8c99f
Chenji-s <s2671252@ed.ac.uk>
2024-10-18 22:30:57 +0100

    11

--- quality_control.py
+++ quality_control.py
@@ -1,1 +1,34 @@
 # Define all functions in this module.
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+def read_image(path, show=False):
+    # 读取 PNG 图像并将其转换为 NumPy 数组
+    img = mpimg.imread(path)
+    
+    if show:
+        # 显示彩色图像
+        plt.imshow(img)
+        plt.title('RGB Image')
+        plt.axis('off')
+        plt.show()
+
+        # 分别显示三个颜色通道：红色、绿色和蓝色
+        fig, axs = plt.subplots(1, 3, figsize=(15, 5))
+
+        axs[0].imshow(img[:, :, 0], cmap='Reds')
+        axs[0].set_title('Red Channel')
+        axs[0].axis('off')
+
+        axs[1].imshow(img[:, :, 1], cmap='Greens')
+        axs[1].set_title('Green Channel')
+        axs[1].axis('off')
+
+        axs[2].imshow(img[:, :, 2], cmap='Blues')
+        axs[2].set_title('Blue Channel')
+        axs[2].axis('off')
+
+        plt.show()
+
+    return img
